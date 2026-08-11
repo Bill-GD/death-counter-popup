da8fe344df52eb27158d1710ab0cc72a8e320fc1
Bill-GD <geometrydashbill@gmail.com>
2026-08-11 16:47:26 +0700

    extract old data parsing to new method

--- src/handlers/DataMigrationHandler.cpp
+++ src/handlers/DataMigrationHandler.cpp
@@ -4,7 +4,51 @@
 #include "utils/FileUtils.hpp"
 #include "utils/Utils.hpp"
 
-void DataMigrationHandler::migrateLevelData(const std::string& levelID) {
+std::map<RunKey, RunData> DataMigrationHandler::parseOldData(const OldDeathCounter& oldData) {
+  std::map<RunKey, RunData> newData{};
+
+  // parse all the old runs into new format
+  for (const auto& [key, count] : oldData) {
+    const auto& [newKey, parentKey] = Utils::computeRunKeys(key);
+    newData[newKey] = RunData{
+      .count = count,
+      .parent = parentKey,
+    };
+  }
+
+  // add RunData for all missing parents
+  std::vector<std::string> keys;
+  keys.reserve(newData.size());
+  for (const auto& [k, _] : newData) {
+    keys.push_back(k);
+  }
+
+  for (int i = 0; i < keys.size(); ++i) {
+    const auto key = keys[i];
+    const auto& runDataIt = newData.find(key);
+    if (runDataIt == newData.end()) continue;
+
+    const auto& [count, parentKey] = runDataIt->second;
+    if (parentKey.empty()) continue;
+
+    if (auto parentIt = newData.find(parentKey); parentIt != newData.end()) {
+      parentIt->second.count += count;
+    } else {
+      newData.emplace(
+        parentKey,
+        RunData{
+          .count = count,
+          .parent = Utils::getParentKey(parentKey),
+        }
+      );
+      keys.push_back(parentKey);
+    }
+  }
+
+  return newData;
+}
+
+void DataMigrationHandler::migrateLevel(const std::string& levelID) {
   const auto& oldPath = SaveHandler::PATH / (levelID + ".json");
   const auto& newPath = SaveHandler::PATH / levelID;
   // ignore linking for now
@@ -16,51 +60,13 @@ void DataMigrationHandler::migrateLevelData(const std::string& levelID) {
     return;
   }
 
-  const auto oldData = Utils::tryParse<OldDeathCounter>(val);
-
   if (const auto res = file::createDirectory(newPath); res.isErr()) {
     log::warn("Failed to create new directory for '{}'", levelID);
     return;
   }
 
-  std::map<RunKey, DeathNodeData> levelDeathData{};
-  for (const auto& [key, count] : oldData) {
-    const auto& [newKey, parentKey] = Utils::computeRunKeys(key);
-    levelDeathData[newKey] = DeathNodeData{
-      .count = count,
-      .parent = parentKey,
-    };
-  }
-
-  std::vector<std::string> keys;
-  keys.reserve(levelDeathData.size());
-  for (const auto& [k, _] : levelDeathData) {
-    keys.push_back(k);
-  }
-
-  for (int i = 0; i < keys.size(); ++i) {
-    const auto key = keys[i];
-    const auto& deathDataIt = levelDeathData.find(key);
-    if (deathDataIt == levelDeathData.end()) continue;
-
-    const auto& [count, parentKey] = deathDataIt->second;
-    if (parentKey.empty()) continue;
-
-    if (auto parentIt = levelDeathData.find(parentKey); parentIt != levelDeathData.end()) {
-      parentIt->second.count += count;
-    } else {
-      levelDeathData.emplace(
-        parentKey,
-        DeathNodeData{
-          .count = count,
-          .parent = Utils::computeRunKeys(parentKey).second,
-        }
-      );
-      keys.push_back(parentKey);
-    }
-  }
-
-  const auto writeSuccess = FileUtils::tryWrite(newPath / "data", levelDeathData);
+  const auto oldData = Utils::tryParse<OldDeathCounter>(val);
+  const auto writeSuccess = FileUtils::tryWrite(newPath / "data", parseOldData(oldData));
   log::info("Migration of level '{}': {}", levelID, writeSuccess ? "Success" : "Fail");
 
   const auto& backupPath = SaveHandler::PATH / "backups" / (levelID + ".json");\
