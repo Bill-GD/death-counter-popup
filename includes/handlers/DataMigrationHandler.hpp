b6a0b1dedca95719ae86eeef8dacdef66be5f387
Bill-GD <geometrydashbill@gmail.com>
2026-08-11 16:47:26 +0700

    extract old data parsing to new method

--- includes/handlers/DataMigrationHandler.hpp
+++ includes/handlers/DataMigrationHandler.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
+#include "types/Run.hpp"
+#include "types/types.hpp"
+
 using namespace geode::prelude;
 
 class DataMigrationHandler {
 public:
-  static void migrateLevelData(const std::string& levelID);
+  static std::map<RunKey, RunData> parseOldData(const OldDeathCounter& oldData);
+  static void migrateLevel(const std::string& levelID);
 };
