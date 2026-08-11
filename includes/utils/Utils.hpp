31463dcd5498ca2f3b3c29df8380a25f21e4011c
Bill-GD <geometrydashbill@gmail.com>
2026-08-11 16:27:03 +0700

    add run init method
    - simple init (internal)
    - init with parents

--- includes/utils/Utils.hpp
+++ includes/utils/Utils.hpp
@@ -19,5 +19,6 @@ public:
   static std::pair<std::string, std::string> split(std::string str, const char delimiter);
   static std::string padToPrecision(std::string str, int len);
   static std::pair<std::string, std::string> computeRunKeys(const std::string& key);
+  static std::string getParentKey(const std::string& key);
   static std::vector<std::string> getAllParentKeys(const std::string& key);
 };
